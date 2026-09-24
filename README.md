363f71edd7da3b23b1a4d1ce94ea3513018513b1
Shervin F. <shervinfashk@gmail.com>
2026-09-24 12:39:39 +0200

    init

--- README.md
+++
@@ -1,2 +0,0 @@
-# shared-lib
-POC: produsere intern pakke, attestere og verifisere pakke før det innsettes i byggeproduksjon
--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-module.exports = 2 + 2;
+
+export const slsaLvl = function (lvl) {
+    var salsas = {
+        1: 'mild',
+        2: 'medium',
+        3: 'hot',
+    };
+    return salsas[lvl] || 'not found';
+}
+
